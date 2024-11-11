3ce91171bee0ea8f7d63653136b53a6de03562be
Shubhi <shubhig389@gmail.com>
2024-11-11 15:46:32 +0530

    updated order

--- app/src/test/java/org/example/order.java
+++ app/src/test/java/org/example/order.java
@@ -2,12 +2,10 @@ package org.example;
 
 import java.time.Duration;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
@@ -73,110 +71,149 @@ String name = fullText.split(" \\(")[0];
 
 // Print the name
 System.out.println("Extracted Name: " + name);
- WebElement prescription=driver.findElement(By.xpath("//*[@id='root']/div[2]/div[2]/div/div[1]/div[2]/div/div/div/div[1]/div[3]/div"));
-                    // Create instance of Javascript executor
-            // JavascriptExecutor js = (JavascriptExecutor) driver;
-            // js.executeScript("arguments[0].scrollIntoView(true);", prescription);
-        Thread.sleep(2000);
-        prescription.click();
+//  WebElement prescription=driver.findElement(By.xpath("//*[@id='root']/div[2]/div[2]/div/div[1]/div[2]/div/div/div/div[1]/div[3]/div"));
+//                     // Create instance of Javascript executor
+//             // JavascriptExecutor js = (JavascriptExecutor) driver;
+//             // js.executeScript("arguments[0].scrollIntoView(true);", prescription);
+//         Thread.sleep(2000);
+//         prescription.click();
       
-        WebElement newPresElement=driver.findElement(By.xpath("//*[@class='btn mt-3 btn-outline-primary']"));
-        Thread.sleep(2000);
-        // js.executeScript("arguments[0].scrollIntoView(true);", newPresElement);
-        newPresElement.click();
+//         WebElement newPresElement=driver.findElement(By.xpath("//*[@class='btn mt-3 btn-outline-primary']"));
+//         Thread.sleep(2000);
+//         // js.executeScript("arguments[0].scrollIntoView(true);", newPresElement);
+//         newPresElement.click();
 
-        Thread.sleep(5000);
+//         Thread.sleep(5000);
 
-     // Select Doctor
-    WebElement selectDoctorElement = driver.findElement(By.xpath("//span[text()='Select Doctor']"));
-    selectDoctorElement.click();
-    Thread.sleep(2000);
+//      // Select Doctor
+//     WebElement selectDoctorElement = driver.findElement(By.xpath("//span[text()='Select Doctor']"));
+//     selectDoctorElement.click();
+//     Thread.sleep(2000);
 
 
-    WebElement selectDoctor = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/span"));
-    Thread.sleep(2000);
-    selectDoctor.click();
+//     WebElement selectDoctor = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/span"));
+//     Thread.sleep(2000);
+//     selectDoctor.click();
 
      
-      // Enter Diagnosis
-    WebElement diagnosisField = driver.findElement(By.xpath("//*[@placeholder='Diagnosis']"));
-    Thread.sleep(2000);
-    diagnosisField.sendKeys("obesity");
+//       // Enter Diagnosis
+//     WebElement diagnosisField = driver.findElement(By.xpath("//*[@placeholder='Diagnosis']"));
+//     Thread.sleep(2000);
+//     diagnosisField.sendKeys("obesity");
 
 
-    // Select Medicine (consider using provided methods for selecting medicine)
-    WebElement selectMedicineElement = driver.findElement(By.xpath("//span[text()='Search Medicine Name']"));
-selectMedicineElement.click();
-Thread.sleep(2000);
+//     // Select Medicine (consider using provided methods for selecting medicine)
+//     WebElement selectMedicineElement = driver.findElement(By.xpath("//span[text()='Search Medicine Name']"));
+// selectMedicineElement.click();
+// Thread.sleep(2000);
 
-    WebElement searchMedicineField = driver.findElement(By.xpath("//*[@placeholder='Search']"));
-    String medicineName = "Tablet Ayaan";
-    searchMedicineField.sendKeys(medicineName);
-    Thread.sleep(2000);
-    // searchMedicineField.sendKeys(Keys.ENTER);
-     Thread.sleep(4000);
-//    driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[1]/span")).click();
-    // **Select medicine from search results (modify logic as needed)**
-    Thread.sleep(2000);
-    // Select Frequency (consider using provided methods for selecting frequency)
-    driver.findElement(By.xpath("//*[@placeholder='Course Duration ']")).sendKeys("30");
-    Thread.sleep(2000);
-    WebElement frequencyElement = driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[2]/div/div[2]/div/div[2]/div[3]/div/select"));
-    Select frequencySelect = new Select(frequencyElement);
-    frequencySelect.selectByVisibleText("Daily");
+//     WebElement searchMedicineField = driver.findElement(By.xpath("//*[@placeholder='Search']"));
+//     String medicineName = "Tablet Ayaan";
+//     searchMedicineField.sendKeys(medicineName);
+//     Thread.sleep(2000);
+//     // searchMedicineField.sendKeys(Keys.ENTER);
+//      Thread.sleep(4000);
+// //    driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[1]/span")).click();
+//     // **Select medicine from search results (modify logic as needed)**
+//     Thread.sleep(2000);
+//     // Select Frequency (consider using provided methods for selecting frequency)
+//     driver.findElement(By.xpath("//*[@placeholder='Course Duration ']")).sendKeys("30");
+//     Thread.sleep(2000);
+//     WebElement frequencyElement = driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[2]/div/div[2]/div/div[2]/div[3]/div/select"));
+//     Select frequencySelect = new Select(frequencyElement);
+//     frequencySelect.selectByVisibleText("Daily");
 
-driver.findElement((By.xpath("//label[text()='Morning']"))).click();
+// driver.findElement((By.xpath("//label[text()='Morning']"))).click();
 
-Thread.sleep(2000);
-    // Select Dosage (consider using provided methods for selecting dosage)
-    WebElement dosageElement = driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[2]/div/div[2]/div/div[2]/div[4]/div/select"));
-    Select dosageSelect = new Select(dosageElement);
-    dosageSelect.selectByVisibleText("1");
+// Thread.sleep(2000);
+//     // Select Dosage (consider using provided methods for selecting dosage)
+//     WebElement dosageElement = driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div[2]/div/div[2]/div/div[2]/div[4]/div/select"));
+//     Select dosageSelect = new Select(dosageElement);
+//     dosageSelect.selectByVisibleText("1");
   
 
 
 
-    WebElement moreDetails= driver.findElement(By.xpath("//*[@id='root']/div[2]/div[2]/div/div[4]/div/div/textarea"));
-    moreDetails.sendKeys("details");
+//     WebElement moreDetails= driver.findElement(By.xpath("//*[@id='root']/div[2]/div[2]/div/div[4]/div/div/textarea"));
+//     moreDetails.sendKeys("details");
     
  
-Thread.sleep(2000);
-    WebElement SaveDetails = driver.findElement(By.xpath("//div[text()='Save Details']"));
-    Thread.sleep(2000);
-    SaveDetails.click();
+// Thread.sleep(2000);
+//     WebElement SaveDetails = driver.findElement(By.xpath("//div[text()='Save Details']"));
+//     Thread.sleep(2000);
+//     SaveDetails.click();
     
-    Thread.sleep(5000);
+//     Thread.sleep(5000);
    
-   WebElement publiElement= driver.findElement(By.xpath("//*[@id='root']/div[2]/div[2]/div/div[1]/div[2]/div/div/div/div[3]/div[1]/div/div/div[1]/div[2]/div[5]/div/span"));
-   Thread.sleep(2000);
-   publiElement.click();
-   Thread.sleep(2000);
-      WebElement publish =driver.findElement(By.xpath("//*[@class='rs-btn rs-btn-primary']"));
-      Thread.sleep(2000);
-    publish.click();
+//    WebElement publiElement= driver.findElement(By.xpath("//*[@id='root']/div[2]/div[2]/div/div[1]/div[2]/div/div/div/div[3]/div[1]/div/div/div[1]/div[2]/div[5]/div/span"));
+//    Thread.sleep(2000);
+//    publiElement.click();
+//    Thread.sleep(2000);
+//       WebElement publish =driver.findElement(By.xpath("//*[@class='rs-btn rs-btn-primary']"));
+//       Thread.sleep(2000);
+//     publish.click();
  
-    //Alert handling
-     // Switch the focus to the alert
-        Alert alert = driver.switchTo().alert();
+//     //Alert handling
+//      // Switch the focus to the alert
+//         Alert alert = driver.switchTo().alert();
 
-        // Get the text of the alert (optional)
-        String alertText = alert.getText();
-        System.out.println("Alert Text: " + alertText);
+//         // Get the text of the alert (optional)
+//         String alertText = alert.getText();
+//         System.out.println("Alert Text: " + alertText);
 
-        // Accept the alert (click OK)
-        alert.accept();
-        Thread.sleep(10000);
+//         // Accept the alert (click OK)
+//         alert.accept();
+//         Thread.sleep(10000);
 }
 
 @Test(priority=4,description="adding order for patient")
 public void addOrder() throws InterruptedException{
     driver.get("https://preweb.joinelevatenow.co.in/orders");
-    Thread.sleep(4000);
+    Thread.sleep(6000);
     driver.findElement(By.xpath("//div[text()='+ Add Order']")).click();
+    Thread.sleep(4000);
+    String name = fullText.split(" \\(")[0];
+    driver.findElement(By.xpath("//input[@placeholder='Search a patient by name or mobile number']")).sendKeys(name);
+    Thread.sleep(4000);
+    driver.findElement(By.xpath("//*[@id='Patients-item-0']")).click();
     Thread.sleep(2000);
-    driver.findElement(By.xpath("//input[@placeholder='Search a patient by name or mobile number']")).sendKeys(fullText);
-    Thread.sleep(2000);
-    // driver.findElement(By.xpath("//*[@id='Patients-item-0']")).click();
-    // Thread.sleep(2000);
-    // driver.findElement(By.xpath("//label[text()='Add New Address']")).click();
-}}
+    driver.findElement(By.xpath("//*[@id='addNewAddress']")).click();
+     // Enter values in the Address fields
+     driver.findElement(By.xpath("//*[@id='root']/div[2]/div[2]/div[2]/div/div[2]/div/div[3]/div[2]/input")).sendKeys("1234 Elm Street");
+   
+     driver.findElement(By.xpath("//*[@placeholder='City']")).sendKeys("Metropolis");
+     driver.findElement(By.xpath("//*[@placeholder='State']")).sendKeys("New York");
+     driver.findElement(By.xpath("//*[@placeholder='Type Pincode']")).sendKeys("123456");
+
+     // Select the address type (e.g., Home)
+    //  WebElement homeType = driver.findElement(By.xpath("//button[text()='home']"));
+    //  homeType.click();
+
+     // Click on 'Save Address' button
+     Thread.sleep(2000);
+     WebElement saveAddressButton = driver.findElement(By.xpath("//button[text()='Save Address']"));
+     saveAddressButton.click();
+
+     System.out.println("New address added successfully!");
+     Thread.sleep(2000);
+
+     driver.findElement(By.xpath("//*[@name='address']")).click();
+     Thread.sleep(2000);
+     WebElement next=driver.findElement(By.xpath("//button[text()='Next']"));
+     next.click();
+     Thread.sleep(5000);
+     String text="";
+     text=driver.findElement(By.xpath("//*[@id='root']/div[2]/div[2]/div[2]/div/div[1]/div[1]/div[1]")).getText();
+     if(text.contains("Manual")){
+        System.out.println("Source is manual");
+     }
+     else{
+        System.out.println("Source is not manual");
+     }
+     driver.findElement(By.xpath("//span[text()='Select']")).click();
+     Thread.sleep(3000);
+     driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[1]/span")).click();
+     Thread.sleep(3000);
+}
+
+}
